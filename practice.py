eb3bad9002573d032b79f576eafe2fa2c9eb527c
jamong <mogja@github.com>
2019-03-25 20:25:24 +0900

    jamong

---
+++ practice.py
@@ -0,0 +1,8 @@
+lion_we_know = ['colin' ,'shawn', 'zayn mailk']
+lion_meet = ['chris' , 'lion' ,'kelly']
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hi")
+    else:
+        print("harry potter")
